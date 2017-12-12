709181e78d60800667dadebecf71dc5880935e13
wangyy <624473068@qq.com>
2017-12-12 12:05:33 +0800

    Initial Commit

---
+++ ZQuanProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQuanProject
+//
+//  Created by wyy on 2017/12/12.
+//  Copyright © 2017年 zquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZQuanProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZQuanProject
+//
+//  Created by wyy on 2017/12/12.
+//  Copyright © 2017年 zquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
