4b1190d65a6bdd6c0e65eb30c2c80c928aea4b51
Army <army8735@qq.com>
2018-02-01 17:39:21 +0800

    Update h5_bridge.js

--- ZQuanProject/h5_bridge.js
+++ ZQuanProject/h5_bridge.js
@@ -63,40 +63,15 @@
       if(name) {
         var event = document.createEvent('Events');
         event.initEvent(name, false, true);
-        if(param !== undefined && param !== null) {
-          var data = {};
-          if(isNumber(param) || isBoolean(param)) {
-            data = param;
-          }
-          else if(isString(param)) {
-            if(param.charAt(0) == '{' && param.charAt(param.length - 1) == '}'
-              || param.charAt(0) == '[' && param.charAt(param.length - 1) == ']') {
-              data = JSON.parse(param);
-            }
-            else {
-              data = param;
-            }
-          }
-          else {
-            data = param;
-          }
-          event.data = data;
-        }
+        event.data = param;
         document.dispatchEvent(event);
       }
     },
     _invokeJS: function(clientId, resp) {
-      console.log("_invokeJS: " + clientId + ", " + typeof resp + ", " + resp);
-      if(resp !== null && resp !== undefined && isString(resp)
-        && (resp.charAt(0) == '{' && resp.charAt(resp.length - 1) == '}'
-          || resp.charAt(0) == '[' && resp.charAt(resp.length - 1) == ']')) {
-        resp = JSON.parse(resp);
-      }
+      console.log("_invokeJS: " + clientId + ", " + resp);
       var func = callbackHash[clientId];
-      setTimeout(function() {
-        func(resp);
-        delete callbackHash[clientId];
-      }, 1);
+      func && func(resp);
+      delete callbackHash[clientId];
     }
   };
 
