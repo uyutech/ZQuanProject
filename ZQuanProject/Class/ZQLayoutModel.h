f440b4c95049f6e5987594ce2d1a8d1a29a310fe
shenhe <shenhe-os@360os.com>
2018-02-01 18:15:24 +0800

    优化

---
+++ ZQuanProject/Class/ZQLayoutModel.h
@@ -0,0 +1,31 @@
+//
+//  ZQLayoutModel.h
+//  ZQuanProject
+//
+//  Created by 王园园 on 2018/2/1.
+//  Copyright © 2018年 zquan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZQLayoutModel : NSObject
+
+@property(nonatomic,assign) BOOL transparentTitle;
+
+@property(nonatomic,assign) BOOL hideBackButton;
+
+@property(nonatomic,assign) BOOL showOptionMenu;
+
+@property(nonatomic,assign) BOOL readTitle;
+
+@property(nonatomic,copy)NSString *title;
+
+@property(nonatomic,copy)NSString *subTitle;
+
+@property(nonatomic,copy)NSString *optionMenu;
+
+@property(nonatomic,copy)NSString *titleBgColor;
+
+@property(nonatomic,copy)NSString *backgroundColor;
+
+@end
---
+++ ZQuanProject/Class/ZQLayoutModel.m
@@ -0,0 +1,13 @@
+//
+//  ZQLayoutModel.m
+//  ZQuanProject
+//
+//  Created by 王园园 on 2018/2/1.
+//  Copyright © 2018年 zquan. All rights reserved.
+//
+
+#import "ZQLayoutModel.h"
+
+@implementation ZQLayoutModel
+
+@end
